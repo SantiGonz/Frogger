8ef9a693aba067c0843f88feb6f2ec32c3ffae86
Santiago Gonzalez <321090412@alumno.enp.unam.mx>
2022-05-29 18:32:18 -0500

    feat(js y html): Añadimos el canvas y y cargamos la imagen

---
+++ dynamics/js/index.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const fondo = new Image();
+fondo.src = "statics/media/images/carretera_fondo2.png";
+
+fondo.addEventListener('load', ()=>{
+    ctx.drawImage(fondo, )
+})
