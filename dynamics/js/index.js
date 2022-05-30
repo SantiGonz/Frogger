68f3fa2495b3b719fa28e827334b36ac70dcce05
Sof Camacho <321199467@alumno.enp.unam.mx>
2022-05-29 19:51:02 -0500

    feat (index.js) Posicionamiento de imagenes

--- dynamics/js/index.js
+++ dynamics/js/index.js
@@ -1,9 +1,24 @@
+//identificación de canvas
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+//identificación de imagenes
 const fondo = new Image();
 fondo.src = "statics/media/images/carretera_fondo2.png";
 
+const amongus = new Image();
+amongus.src = "statics/media/images/amongus_sprites.png";
+
+//se muestran las imagenes
 fondo.addEventListener('load', ()=>{
-    ctx.drawImage(fondo, )
+    ctx.drawImage(fondo, 380, 320);    
 })
+
+amongus.addEventListener('load', () => {
+    ctx.drawImage(amongus, 5, 5, 52, 55, 340, 320, 52, 50);
+    ctx.drawImage(amongus, 62, 70, 52, 55, 340, 390, 52, 50);
+});
+
+
+
+
